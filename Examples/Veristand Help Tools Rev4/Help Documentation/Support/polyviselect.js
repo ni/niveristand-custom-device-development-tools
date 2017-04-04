0d415a630db2b48d097804c12e788d5f687a85f0
Peter George <peter.george@ni.com>
2017-04-04 16:38:32 -0500

    Revert "Example project"
    This reverts commit 28198275faed42dc8bf2c8e70bbd7bac57f0a567.

--- Examples/Veristand Help Tools Rev4/Help Documentation/Support/polyviselect.js
+++
@@ -1,6 +0,0 @@
-function polyVISelectHandler(selectObj) 
-{
-   window.location = selectObj.options[selectObj.selectedIndex].value;
-   selectObj.selectedIndex = 0; //To put the selector back on "Select an instance"
-   window.focus(); //To remove focus from the selector so that using the scroll wheel doesn't change poly VIs
-}
